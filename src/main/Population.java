122ab68c7d564da1b569cd436212af34663c2e18
naikv <naikv@rose-hulman.edu>
2021-02-10 19:51:42 -0500

    Did some work in population class

--- src/main/Population.java
+++ src/main/Population.java
@@ -8,9 +8,17 @@ public class Population {
 	
 	public Population() {
 		for (int i = 0; i < 100; i++) {
-			
+			this.chromosomes.add(new Chromosome(100));
 		}
 	}
+	
+	public Population(int numChromosomes, int numGenes) {
+		for (int i = 0; i < numChromosomes; i++) {
+			this.chromosomes.add(new Chromosome(numGenes));
+		}
+	}
+	
+	
 	
 
 }
