92229b0befecadcc616089b374c5af0701a87223
naikv <naikv@rose-hulman.edu>
2021-02-10 23:56:23 -0500

    Milestone 2 almost done

---
+++ src/main/StartListener.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StartListener implements ActionListener{
+	private EvolutionMain main;
+	private EvolutionComponent component;
+	
+	public StartListener(EvolutionMain main, EvolutionComponent component) {
+		this.main = main;
+		this.component = component;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		this.main.setPop(new Population());
+		this.main.setGenerations(200);
+		this.component.clear();
+	}
+
+}
---
+++ src/main/TimerListener.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TimerListener implements ActionListener {
+
+	private EvolutionMain main;
+	private EvolutionComponent c;
+
+	public TimerListener(EvolutionMain main, EvolutionComponent c) {
+		this.main = main;
+		this.c = c;
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (this.main.getPop() != null && this.main.getGenerations() > 0) {
+			this.main.getPop().fitnessSort();
+			this.c.addEntry(this.main.getPop().getChromosomes().get(0).FitnessValue(),
+					this.main.getPop().getChromosomes().get(this.main.getPop().getChromosomes().size() - 1).FitnessValue(),
+					this.main.getPop().getChromosomes().get(this.main.getPop().getChromosomes().size() / 2).FitnessValue());
+			this.main.getPop().evolutionLoop(1);
+			this.c.addEntry(this.main.getPop().getChromosomes().get(0).FitnessValue(),
+					this.main.getPop().getChromosomes().get(this.main.getPop().getChromosomes().size() - 1).FitnessValue(),
+					this.main.getPop().getChromosomes().get(this.main.getPop().getChromosomes().size() / 2).FitnessValue());
+			this.main.setGenerations(this.main.getGenerations() - 1);
+
+		}
+		c.repaint();
+
+	}
+
+}
