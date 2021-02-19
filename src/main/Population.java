fbfb36b3486af996a37ed21e25fd1088bf1135b5
chongw <chongw@137.112.228.99>
2021-02-19 01:47:35 -0500

    roulette complete

--- src/main/Population.java
+++ src/main/Population.java
@@ -16,6 +16,7 @@ public class Population {
 	private int elitismRate;
 	private int fitnessSelect;
 	private boolean crossover;
+	private boolean roulette;
 	private ArrayList<Chromosome> chromosomes = new ArrayList<>();
 
 	public Population() {
@@ -87,52 +88,114 @@ public class Population {
 	public void evolutionLoop(int mutationRate) throws FileNotFoundException {
 		fitnessSort();
 		int startingSize = this.chromosomes.size();
-		int total = 0;
-		for (int i = 0; i < startingSize; i++) {
-			total += this.chromosomes.get(i).FitnessValue(this.fitnessSelect);
-		}
-		int sumProb = 0;
-		for (int i = 0; i < startingSize; i++) {
-		}
+		Chromosome c;
 
-		for (int i = 0; i < startingSize / 2; i++) {
-			chromosomes.remove(0);
-		}
-		int clones = (elitismRate / 2);
-		startingSize = this.chromosomes.size();
+		if (roulette) { // Roulette code
+			int clones = (elitismRate * startingSize) / 100;
+			for (int i = 0; i < startingSize - clones; i++) {
+				
+				if (crossover) {
+					int chromosome1 = getIndex(rouletteResult(startingSize));
+					int chromosome2 = getIndex(rouletteResult(startingSize));
+					c = crossoverResult(chromosome1, chromosome2);
+				} else {
+					c = rouletteResult(startingSize);
+				}
+				c.mutate(mutationRate);
+				chromosomes.add(c);
+			}
+			for (int i = 0; i < startingSize - clones; i++) {
+				chromosomes.remove(0);
+			}
 
-		for (int i = 0; i < 2 * startingSize - clones; i++) {
-			Chromosome c;
-			if(crossover) {
-				int chromosome1 = (int)(chromosomes.size()*Math.random());
-				int chromosome2 = (int)(chromosomes.size()*Math.random());
-				c = crossoverGenes(chromosome1, chromosome2);
+		} else {// Truncation code
+			for (int i = 0; i < startingSize / 2; i++) {
+				chromosomes.remove(0);
 			}
-			else{
-				c = new Chromosome(chromosomes.get(i / 2).getGenes());
+			int clones = (elitismRate / 2);
+			startingSize = this.chromosomes.size();
+
+			for (int i = 0; i < 2 * startingSize - clones; i++) {
+				if (crossover) {
+					int chromosome1 = (int) (chromosomes.size() * Math.random());
+					int chromosome2 = (int) (chromosomes.size() * Math.random());
+					c = crossoverResult(chromosome1, chromosome2);
+				} else {
+					c = new Chromosome(chromosomes.get(i / 2).getGenes());
+				}
+				c.mutate(mutationRate);
+				chromosomes.add(c);
 			}
-			c.mutate(mutationRate);
-			chromosomes.add(c);
-		}
-		for (int i = 0; i < startingSize - clones; i++) {
-			chromosomes.remove(0);
+			for (int i = 0; i < startingSize - clones; i++) {
+				chromosomes.remove(0);
+			}
 		}
 	}
 
-	public Chromosome crossoverGenes(int chromosome1, int chromosome2){
+	
+	public int getIndex(Chromosome c) {
+		for(int i = 0; i < this.chromosomes.size(); i ++) {
+			if(this.chromosomes.get(i)==c) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public Chromosome rouletteResult(int startingSize) {
 		Chromosome c;
-		ArrayList <Character> newGenes = new ArrayList<>();
-		int crosspoint = (int)(chromosomes.get(0).getGenes().size()*Math.random());
+		double total = 0;
+		int sumProb = 0;
+		HashMap<Chromosome, Double> probabilities = new HashMap<>();
+		for (int i = 0; i < startingSize; i++) {
+			try {
+				total += this.chromosomes.get(i).FitnessValue(this.fitnessSelect);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
 
-		for(int j = 0; j < crosspoint; j ++) {
+		for (int i = 0; i < startingSize; i++) {
+			double prob = 0;
+			try {
+				prob = this.chromosomes.get(i).FitnessValue(this.fitnessSelect) / total;
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			if (probabilities.isEmpty()) {
+				probabilities.put(this.chromosomes.get(i), prob);
+			} else {
+				probabilities.put(this.chromosomes.get(i), probabilities.get(chromosomes.get(i - 1)) + prob);
+			}
+		}
+		double finalValue = Math.random();
+		for (int i = 0; i < startingSize; i++) {
+			if (i == 0) {
+				if (probabilities.get(chromosomes.get(0)) > finalValue) {
+					return c = new Chromosome(chromosomes.get(0).getGenes());
+				}
+			} else if (probabilities.get(chromosomes.get(i - 1)) < finalValue
+					&& probabilities.get(chromosomes.get(i)) > finalValue) {
+				return c = new Chromosome(chromosomes.get(i).getGenes());
+			}
+		}
+		return c = new Chromosome(chromosomes.get(startingSize - 1).getGenes());
+	}
+
+	public Chromosome crossoverResult(int chromosome1, int chromosome2) {
+		Chromosome c;
+		ArrayList<Character> newGenes = new ArrayList<>();
+		int crosspoint = (int) (chromosomes.get(0).getGenes().size() * Math.random());
+
+		for (int j = 0; j < crosspoint; j++) {
 			newGenes.add(chromosomes.get(chromosome1).getGenes().get(j));
 		}
-		for(int j = crosspoint; j < chromosomes.get(0).getGenes().size(); j ++) {
+		for (int j = crosspoint; j < chromosomes.get(0).getGenes().size(); j++) {
 			newGenes.add(chromosomes.get(chromosome2).getGenes().get(j));
 		}
 		c = new Chromosome(newGenes);
 		return c;
 	}
+
 	public ArrayList<Chromosome> getChromosomes() {
 		return this.chromosomes;
 	}
